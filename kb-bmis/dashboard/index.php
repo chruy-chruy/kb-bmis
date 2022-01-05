9fecb96c259bba91baac45d7f50ee4e138b8fb0d
herzliyaaa <herzliabarangan@gmail.com>
2022-01-06 02:38:26 +0800

    added data in dashboard

--- kb-bmis/dashboard/index.php
+++ kb-bmis/dashboard/index.php
@@ -1,96 +1,105 @@
 <?php
 $headerTitle = 'Dashboard';
 $page = 'Dashboard';
+
 require_once "../../includes/header.php";
+include "../../db_conn.php";
+include "data-dashboard.php";
+
 ?>
 
-    <!--=============== MAIN ===============-->
-    <main>
+<!--=============== MAIN ===============-->
+<main>
 
-      <!-- ABOUT -->
-      
+    <!-- ABOUT -->
 
-      <!-- DASHBOARD -->
-      <div class="content" style="display:block;">
-        
+
+    <!-- DASHBOARD -->
+    <div class="content" style="display:block;">
+
         <section class="dashboard">
-        <div class="dashboard__logo">
-          <img src="../../assets/img/north-logo.png" alt="">
-        </div>
-
-
-        <div class="dashboard__cards-top">
-            <div class="dashboard__card-top">
-                <div>
-                  <div class="dashboard__card-top--value">10734</div>
-                  <div class="dashboard__card-top--label">Population</div>
-                </div>
-                
-                <span class="dashboard__card-top--icon"><i class='bx bx-group'></i></span>
+            <div class="dashboard__logo">
+                <img src="../../assets/img/north-logo.png" alt="">
             </div>
 
-            <div class="dashboard__card-top">
-                <div>
-                  <div class="dashboard__card-top--value">7345</div>
-                  <div class="dashboard__card-top--label">Registered Voters</div>
+
+            <div class="dashboard__cards-top">
+                <div class="dashboard__card-top">
+                    <div>
+                        <div class="dashboard__card-top--value"><?php echo $countPopulation ?></div>
+                        <div class="dashboard__card-top--label">Population</div>
+                    </div>
+
+                    <span class="dashboard__card-top--icon"><i class='bx bx-group'></i></span>
                 </div>
 
-                <span class="dashboard__card-top--icon"><i class='bx bx-box'></i></span>
-            </div>
+                <div class="dashboard__card-top">
+                    <div>
+                        <div class="dashboard__card-top--value"><?php echo $countVoters['voters']; ?></div>
+                        <div class="dashboard__card-top--label">Registered Voters</div>
+                    </div>
 
-            <div class="dashboard__card-top">
-                <div>
-                  <div class="dashboard__card-top--value">4192</div>
-                  <div class="dashboard__card-top--label">Happy</div>
+                    <span class="dashboard__card-top--icon"><i class='bx bx-box'></i></span>
                 </div>
-                
-                <span class="dashboard__card-top--icon"><i class='bx bx-happy-alt' ></i></span>
-            </div>
 
-            <div class="dashboard__card-top">
-                <div>
-                  <div class="dashboard__card-top--value">6542</div>
-                  <div class="dashboard__card-top--label">Angry</div>
+                <div class="dashboard__card-top">
+                    <div>
+                        <div class="dashboard__card-top--value">₱<?php echo $total_sales['brgySales']; ?></div>
+                        <div class="dashboard__card-top--label">Barangay Clearance Total Sales</div>
+                    </div>
+
+                    <span class="dashboard__card-top--icon"><i class='bx bx-happy-alt'></i></span>
                 </div>
-                
-                <span class="dashboard__card-top--icon"><i class='bx bx-angry' ></i></span>
-            </div>
-          </div>
 
-          <!-- <hr> -->
+                <div class="dashboard__card-top">
+                    <div>
+                        <div class="dashboard__card-top--value">₱6542</div>
+                        <div class="dashboard__card-top--label">Business Clearance Total Sales</div>
+                    </div>
 
-          <!-- CHARTS -->
-          <div class="dashboard__cards-bottom">
-            <div class="dashboard__card-bottom dashboard__card-bottom--card1">
-              <canvas id="myChart"></canvas>
+                    <span class="dashboard__card-top--icon"><i class='bx bx-angry'></i></span>
+                </div>
             </div>
-            <div class="dashboard__card-bottom dashboard__card-bottom--card2">
-              <canvas id="myChart2"></canvas>
-            </div>
-            <!-- <div class="grid grid--3">
+
+            <!-- <hr> -->
+
+
+            <!-- CHARTS -->
+            <div class="dashboard__cards-bottom">
+                <div class="dashboard__card-bottom dashboard__card-bottom--card1">
+                    <canvas id="myChart"></canvas>
+                </div>
+                <div class="dashboard__card-bottom dashboard__card-bottom--card2">
+                    <canvas id="myChart2"></canvas>
+                </div>
+                <!-- <div class="grid grid--3">
             </div> -->
-          </div>
+            </div>
 
-          <!-- <span class="dashboard__down-arrow">
+            <!-- <span class="dashboard__down-arrow">
             <i class='bx bxs-down-arrow bx-fade-up' ></i>
           </span> -->
         </section>
-        
+
         <!-- <section class="dashboard__notes">
           Hello, North!
         </section> -->
-      </div>
+    </div>
 
-      <!-- RESIDENTS -->
-      
-      
-      
-    </main>
+    <!-- RESIDENTS -->
 
-    <!--=============== SCROLL UP ===============-->
 
-    <!--=============== CHART JS ===============-->
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.5.1/dist/chart.min.js"></script>
 
-  </body>
+</main>
+
+<!--=============== SCROLL UP ===============-->
+
+
+<!--=============== CHART JS ===============-->
+<script src="https://cdn.jsdelivr.net/npm/chart.js@3.5.1/dist/chart.min.js"></script>
+
+<?php include "charts.php"; ?>
+
+</body>
+
 </html>
